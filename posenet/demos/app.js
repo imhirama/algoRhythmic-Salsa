928ad8ff1d28d8056f64f57c333284b4703e5a42
Isabel Hirama <isabel.hirama@gmail.com>
2018-08-05 23:27:45 -0400

    Edited dist settings

--- posenet/demos/app.js
+++ posenet/demos/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 let app = express();
-if(process.env.NODE_ENV !== 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('dist'));
 } else {
     app.use(express.static('public'));
@@ -11,7 +11,7 @@ if(process.env.NODE_ENV !== 'production'){
 
 
 app.get('/', (req, res)=>{
-    if(process.env.NODE_ENV !== 'production'){
+    if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.join(__dirname+'/dist/index.html'));
     } else {
         res.sendFile(path.join(__dirname+'/public/index.html'));
